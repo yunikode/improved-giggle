6ea4446f672f729af9cf6e9151bdacd548ef978c
zee-german <franz.lutzenberger@gmail.com>
2017-01-07 17:29:23 +0100

    show present users

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -1,12 +1,22 @@
 defmodule Vuechat.RoomChannel do
   use Vuechat.Web, :channel
+  alias Vuechat.Presence
 
   def join("room:lobby", payload, socket) do
+    send(self, :after_join)
     {:ok, socket}
   end
 
   def join("room:" <> _private_room_id, _params, _socket) do
     {:error, %{reason: "Unauthorized"}}
+  end
+
+  def handle_info(:after_join, socket) do
+    {:ok, _} = Presence.track(socket, socket.assigns.username, %{
+      online_at: inspect(System.system_time(:milliseconds))
+      })
+    push socket, "presence_state", Presence.list(socket)
+    {:noreply, socket}
   end
 
   def handle_in("new_msg", %{"body" => body}, socket) do
--- web/static/components/my-app.vue
+++ web/static/components/my-app.vue
@@ -1,26 +1,36 @@
 <template>
 <div class="my-app">
-  <h1>Vuechat</h1>
   <div class="user-detail" v-if='enterName'>
+    <h1>Vuechat</h1>
     <label for="userName">Please enter your name:</label> <br>
     <input type="text" v-model="username" name="userName">
     <button v-on:click="connectToChat">Next</button>
   </div>
   <div class="messages" v-else>
+    Online users:
+    <ul v-for="user in users">
+      <li>
+        {{ user.user }} ({{ user.online_at}})
+      </li>
+    </ul>
+    Messages:
     <ul v-for='message in messages'>
       <li>
         <small>{{ message.received_at }}</small>: <strong>{{ message.username }}</strong>: {{ message.body }}
       </li>
     </ul>
-    <strong>{{ username }}</strong>
-    <input type="text" v-model="message" v-on:keyup.13="sendMessage">
+    <p>
+      <strong>{{ username }}: </strong><br>
+      <input type="text" placeholder="What do have to say?" v-model="message" v-on:keyup.13="sendMessage">
+    </p>
   </div>
 </div>
 </template>
 
 <script>
 import {
-  Socket
+  Socket,
+  Presence
 } from "phoenix"
 
 export default {
@@ -31,7 +41,8 @@ export default {
       messages: [],
       message: '',
       username: '',
-      enterName: true
+      enterName: true,
+      users: []
     }
   },
   methods: {
@@ -42,6 +53,7 @@ export default {
       this.message = ''
     },
     connectToChat() {
+      let presences = {}
       this.enterName = false
       this.socket = new Socket('/socket', {
         params: {
@@ -51,9 +63,19 @@ export default {
       this.socket.connect()
 
       this.channel = this.socket.channel("room:lobby", {})
-      this.channel.on('new_msg', payload => {
+      this.channel.on("new_msg", payload => {
         payload.received_at = Date()
         this.messages.push(payload)
+      })
+
+      this.channel.on("presence_state", state => {
+        presences = Presence.syncState(presences, state)
+        this.assignUsers(presences)
+      })
+
+      this.channel.on("presence_diff", diff => {
+        presences = Presence.syncDiff(presences, diff)
+        this.assignUsers(presences)
       })
 
       this.channel.join()
@@ -64,18 +86,34 @@ export default {
           console.log('Unable to join', response)
         })
     },
+    assignUsers(presences) {
+      let formatTimestamp = (timestamp) => {
+        timestamp = parseInt(timestamp)
+        let date = new Date(timestamp)
+        return date.toLocaleTimeString()
+      }
+      this.users = Presence.list(presences, (user, {
+        metas: metas
+      }) => {
+        return {
+          user: user,
+          online_at: formatTimestamp(metas[0].online_at)
+        }
+      })
+    }
   }
 }
 </script>
 
+
 <style lang="sass">
 .my-app {
-  margin-left: auto;
-  margin-right: auto;
-  width: 800px;
+    margin-left: auto;
+    margin-right: auto;
+    width: 800px;
 
-  h1 {
-      text-align: center;
-  }
+    h1 {
+        text-align: center;
+    }
 }
 </style>
