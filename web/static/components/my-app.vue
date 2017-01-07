7d7606cd43278a503cc930ad5c809d2a2d8d2de8
zee-german <franz.lutzenberger@gmail.com>
2017-01-07 10:18:13 +0100

    first working prototype

---
+++ web/static/components/my-app.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="my-app">
+    <h1>Vuechat</h1>
+    <ul v-for='message in messages'>
+      <li>
+        <small>{{ message.received_at }}</small>: {{ message.body }}
+      </li>
+    </ul>
+
+    <input type="text" v-model="message" v-on:keyup.13="sendMessage">
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: ''
+    }
+  },
+  methods: {
+    sendMessage() {
+      this.$parent.channel.push('new_msg', { body: this.message })
+      this.message = ''
+    }
+  },
+  computed: {
+    messages() {
+      return this.$parent.messages
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+  .my-app {
+    margin-left: auto;
+    margin-right: auto;
+    width: 800px;
+
+    h1 {
+      text-align: center;
+    }
+  }
+</style>
