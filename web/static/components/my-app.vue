8cd08495590387d837908aac81fea2d638dc1bcf
zee-german <franz.lutzenberger@gmail.com>
2017-01-07 16:04:08 +0100

    display username in chat message

--- web/static/components/my-app.vue
+++ web/static/components/my-app.vue
@@ -1,45 +1,81 @@
 <template>
-  <div class="my-app">
-    <h1>Vuechat</h1>
+<div class="my-app">
+  <h1>Vuechat</h1>
+  <div class="user-detail" v-if='enterName'>
+    <label for="userName">Please enter your name:</label> <br>
+    <input type="text" v-model="username" name="userName">
+    <button v-on:click="connectToChat">Next</button>
+  </div>
+  <div class="messages" v-else>
     <ul v-for='message in messages'>
       <li>
-        <small>{{ message.received_at }}</small>: {{ message.body }}
+        <small>{{ message.received_at }}</small>: <strong>{{ message.username }}</strong>: {{ message.body }}
       </li>
     </ul>
-
+    <strong>{{ username }}</strong>
     <input type="text" v-model="message" v-on:keyup.13="sendMessage">
   </div>
+</div>
 </template>
 
 <script>
+import {
+  Socket
+} from "phoenix"
+
 export default {
   data() {
     return {
-      message: ''
+      socket: null,
+      channel: null,
+      messages: [],
+      message: '',
+      username: '',
+      enterName: true
     }
   },
   methods: {
     sendMessage() {
-      this.$parent.channel.push('new_msg', { body: this.message })
+      this.channel.push('new_msg', {
+        body: this.message
+      })
       this.message = ''
-    }
-  },
-  computed: {
-    messages() {
-      return this.$parent.messages
-    }
+    },
+    connectToChat() {
+      this.enterName = false
+      this.socket = new Socket('/socket', {
+        params: {
+          username: this.username
+        }
+      })
+      this.socket.connect()
+
+      this.channel = this.socket.channel("room:lobby", {})
+      this.channel.on('new_msg', payload => {
+        payload.received_at = Date()
+        this.messages.push(payload)
+      })
+
+      this.channel.join()
+        .receive('ok', response => {
+          console.log('joined successfully', response)
+        })
+        .receive('error', response => {
+          console.log('Unable to join', response)
+        })
+    },
   }
 }
 </script>
 
 <style lang="sass">
-  .my-app {
-    margin-left: auto;
-    margin-right: auto;
-    width: 800px;
+.my-app {
+  margin-left: auto;
+  margin-right: auto;
+  width: 800px;
 
-    h1 {
+  h1 {
       text-align: center;
-    }
   }
+}
 </style>
--- web/static/js/app.js
+++ web/static/js/app.js
@@ -18,4 +18,17 @@ import "phoenix_html"
 // Local files can be imported directly using relative
 // paths "./socket" or full ones "web/static/js/socket".
 
-import socket from "./socket"
+import Vue from 'vue'
+import MyApp from '../components/my-app.vue'
+
+// Create the main component
+Vue.component('my-app', MyApp)
+
+// and create the top-level view model
+
+new Vue({
+  el: '#app',
+  render (createElement) {
+    return createElement(MyApp, {})
+  }
+})
